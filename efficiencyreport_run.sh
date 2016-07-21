d5fd388d4e57c8da2cada6335e883f9b5111bce6
sbhat <sbhat@fnal.gov>
2016-07-21 09:17:03 -0500

    Removed weird git junk in efficiencyreport_run.sh

--- efficiencyreport_run.sh
+++ efficiencyreport_run.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 VOS="NOvA SeaQuest MINERvA MINOS gm2 Mu2e UBooNe DarkSide DUNE CDMS MARS CDF" 
-=======
-VOS="NOvA Seaquest Minerva Minos gm2 Mu2e UBooNe Darkside DUNE CDMS Mars CDF" 
->>>>>>> d6f4da5b8bb87c8e397a7471591594a06647a1c2
 YESTERDAY=`date --date yesterday +"%F %T"`
 TODAY=`date +"%F %T"`
 
